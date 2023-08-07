3900e5bc62eb91f242695e3478471d6156a05214
Shadab <shadabjhu00447@gmail.com>
2023-08-07 09:33:50 +0530

    stdfunc

---
+++ src/main/kotlin/StdFunctions.kt
@@ -0,0 +1,43 @@
+fun printCar(car: Car?) {
+    val isCoupe = car?.let {
+        (it.doors <= 2)
+    }
+    if (isCoupe == true) {
+        println("Coupes are awesome")
+    }
+}
+fun printCar(car: Car?) {
+    val isCoupe = car?.run {
+        (this.doors <= 2)
+    }
+    if (isCoupe == true) {
+        println("Coupes are awesome")
+    }
+}
+fun printCar3(car: Car?) {
+    car?.also {
+        it.doors = 4
+    }.let {
+        if (it?.doors != null && it.doors <= 2) {
+            println("Coupes are awesome")
+        }
+    }
+}
+
+fun printCar4(car: Car?) {
+    car?.apply {
+        doors = 4
+    }.let {
+        if (it?.doors != null && it.doors <= 2) {
+            println("Coupes are awesome")
+        }
+    }
+}
+
+
+
+fun main(){
+
+
+}
+
